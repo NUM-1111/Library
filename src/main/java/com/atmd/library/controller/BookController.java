150b3d59c46377934c754ef4550c14e2d12bef28
NUM-1111 <2943603313@qq.com>
2025-10-14 20:08:59 +0800

    完善controller的方法,保证每一个方法都可用postman测试

--- src/main/java/com/atmd/library/controller/BookController.java
+++ src/main/java/com/atmd/library/controller/BookController.java
@@ -5,6 +5,7 @@ import com.atmd.library.domain.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -42,4 +43,32 @@ public class BookController {
     public void deleteBook(@PathVariable String isbn) throws Exception {
         bookService.deleteBook(isbn);
     }
+
+    // PUT /books/{isbn}
+    @PutMapping("/{isbn}")
+    public Book updateBook(@PathVariable String isbn, @RequestBody Book bookDetails) throws Exception{
+        // 确保URL中的isbn和请求体中的isbn一致，或以URL为准
+        bookDetails.setIsbn(isbn);
+        return bookService.updateBook(bookDetails);
+    }
+
+
+// GET /books/search?title=some_title
+// 或
+// GET /books/search?author=some_author
+    @GetMapping("/search")
+    public List<Book> searchBooks(
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "author", required = false) String author) {
+
+        // 根据传入的参数决定调用哪个服务
+        if (title != null) {
+            return bookService.getBooksByTitle(title);
+        } else if (author != null) {
+            return bookService.getBookByAuthor(author);
+        }
+        // 如果没有提供任何参数，可以返回一个空列表或抛出异常
+        return new ArrayList<>();
+    }
+
 }
